1ae1d6866f6f211a3a98574e4c3b70fbac16a2ab
ZU <zshaihova@gmail.com>
2023-04-02 19:16:10 +0300

    добавила еще одну строчку в Ветка_1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("тест 2, ветка 1");
-        System.out.printf("+ еще изменения");
+        System.out.print("+ еще изменения");
+        System.out.print("+ и еще изменения");
     }
 }
